48e678af33f648f6c2388f4107b12a63a9e43903
d8p87 <mynewiphone62020@gmail.com>
2026-01-06 00:35:50 +0300

    Tryin to merge

--- src/SubmitPlace.jsx
+++ src/SubmitPlace.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./App.css";
 
-const API_BASE = "http://localhost:3001";
+const API_BASE = "";
 
 const emptyForm = {
   name: "",
